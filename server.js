2ac46395718fe0e8d4e7481e5a81b781e0119fb8
AsafDavidov <adavidov@conncoll.edu>
2019-02-01 19:03:40 -0500

    Basic post functionality

--- server.js
+++ server.js
@@ -3,14 +3,33 @@
 const http         = require('http');
 const finalhandler = require('finalhandler');
 const Router       = require('router');
+const bodyParser = require('body-parser');
+let messages = [];
+let nextId = 1;
+
+class Message {
+  constructor(message) {
+    this.id = nextId;
+    this.message = message;
+    nextId++;
+  }
+}
 
 const router = new Router();
+router.use(bodyParser.json());
 
 router.get('/', (request, response) => {
   // A good place to start!
   response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
   response.write("Hello, World!")
   response.end();
+});
+
+router.post('/message',(request,response)=>{
+  response.setHeader('Content-Type', 'application/json; charset=utf-8');
+  let newMessage = new Message(request.body.message)
+  messages.push(newMessage)
+  response.end(JSON.stringify(newMessage.id));
 });
 
 const server = http.createServer((request, response) => {
